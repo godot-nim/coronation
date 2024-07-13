a29d81ac4662dd7b40d1533bfdedd28250230de8
la .panon. <panno.800k@gmail.com>
2024-07-13 22:58:08 +0900

    Omit the return side of the getter.
    Mainly because multiple types may be set, e.g., for shader materials.

--- src/coronationpkg/operators/classes/properties.nim
+++ src/coronationpkg/operators/classes/properties.nim
@@ -14,7 +14,6 @@ proc weave_properties*(class: Class): Cloth =
         let index_get = (if prop.index.isSome: $prop.index.get else: "")
         let index_set = (if prop.index.isSome: $prop.index.get & ", " else: "")
         let name = prop.name.scan.convert(ProcSym)
-        let retT = prop.`type`.scan.convert(TypeSym)
-        &"template {name}*(self: {class.typesym}): {retT} = self.{prop.getter.scan.convert(ProcSym)}({index_get})"
+        &"template {name}*(self: {class.typesym}): untyped = self.{prop.getter.scan.convert(ProcSym)}({index_get})"
         if prop.setter.isSome:
           &"template `{name}=`*(self: {class.typesym}; value) = self.{prop.setter.get.scan.convert(ProcSym)}({index_set}value)"
